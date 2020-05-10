16990b871a13962feef985826b219a83ff24b0fe
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-05-09 21:17:32 -0400

    Update php7.2.sh

--- php7.2.sh
+++ php7.2.sh
@@ -96,7 +96,7 @@ sed -i 's,^file_uploads =.*$,file_uploads = On,' php.ini
 sed -i 's,^allow_url_fopen =.*$,allow_url_fopen = On,' php.ini
 sed -i 's,^short_open_tag =.*$,short_open_tag = On,' php.ini
 sed -i 's,^memory_limit =.*$,memory_limit = 10G,' php.ini
-sed -i 's,^upload_max_filesize =.*$,upload_max_filesize = 2000M,' php.ini
+sed -i 's,^upload_max_filesize =.*$,upload_max_filesize = 1000M,' php.ini
 sed -i 's,^max_execution_time =.*$,max_execution_time = 18000,' php.ini
 sed -i 's,^date.timezone =.*$,date.timezone = America/Chicago,' php.ini
 
