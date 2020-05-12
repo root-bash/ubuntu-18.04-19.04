839dd3b59dec31d0e5544b9d630c591622fca895
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-05-12 02:58:14 -0400

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -16,6 +16,35 @@ systemctl enable mysql.service
 
 mysql_secure_installation
 
+echo ""
+echo "Enter the mysql user password: ----"
+echo ""
+read PASSWORD
+echo ""
+
+# To create an new user id and password in the database
+echo ""
+echo "Enter the new user id: ----"
+echo ""
+read CREATE_USER_NAME
+echo ""
+echo ""
+echo "Enter the new user password: ----"
+echo ""
+read USER_PASSWORD
+echo ""
+echo ""
+mysql -u USER -p$PASSWORD -e "CREATE USER '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
+mysql -u USER -p$PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY PASSWORD '$USER_PASSWORD';"
+
+
+echo ""
+echo "Please Enter the database name: ----"
+echo ""
+read DB
+mysql -u USER -p$PASSWORD -e "CREATE DATABASE $DB;"
+
+
 
 
 echo "---------------------------------------------"
