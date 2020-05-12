392f374e053c2c3c241d440e25791b55cf7d30c8
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-05-12 03:16:19 -0400

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -16,6 +16,42 @@ systemctl enable mariadb.service
 
 mysql_secure_installation
 
+
+clear
+echo ""
+echo ""
+echo ""
+echo "***************************************************************************"
+echo "         Please complete the configuration in the database: ----           "
+echo "***************************************************************************"
+echo ""
+echo "Enter the database root password: ----"
+echo ""
+read PASSWORD
+echo ""
+
+# To create an new user id and password in the database
+echo ""
+echo "Enter the new user id: ----"
+echo ""
+read CREATE_USER_NAME
+echo ""
+echo ""
+echo "Enter the new user password: ----"
+echo ""
+read USER_PASSWORD
+echo ""
+echo ""
+mysql -u USER -p$PASSWORD -e "CREATE USER '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
+mysql -u USER -p$PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
+
+
+echo ""
+echo "Please Enter the database name: ----"
+echo ""
+read DB
+mysql -u USER -p$PASSWORD -e "CREATE DATABASE $DB;"
+
 echo "---------------------------------------------"
 echo ""
 echo ""
@@ -23,3 +59,7 @@ echo "       MariaDB installation has finished"
 echo ""
 echo ""
 echo "---------------------------------------------"
+echo ""
+echo ""
+echo ""
+echo ""
