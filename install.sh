51074359f4ab1656feb2cb654d4468cb29a0ad59
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-05-26 20:02:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ chmod -R 777 mariadb.sh
 clear
 echo "#############################################################"
 echo ""
-echo "              Please select the service......"
+echo -e "   \e[1;32mPlease select the service......  \e[0m  "
 echo ""
 echo "#############################################################"
 
@@ -22,37 +22,37 @@ do
     case $opt in
         "DNS Service")
         	echo "#############################################################"
-            echo "       DNS Service is going to install in the server     "
+            echo -e "   \e[1;32mDNS Service is going to install in the server: ----  \e[0m  "
             echo "#############################################################"      
             ./dns.sh
             ;;
         "Web Service")
         	echo "#############################################################"
-            echo "       WEB Service is going to install in the server     "
+            echo -e "   \e[1;32mWEB Service is going to install in the server: ----  \e[0m  "
             echo "#############################################################"
             ./apache2.sh
             ;;
         "PHP 7.2")
             echo "#############################################################"
-            echo "       PHP 7.2 Service is going to install in the server     "
+            echo -e "   \e[1;32mPHP 7.2 Service is going to install in the server: ----  \e[0m  "
             echo "#############################################################" 
             ./php7.2.sh
             ;;
         "PHP 7.3")
             echo "#############################################################"
-            echo "       PHP 7.3 Service is going to install in the server     "
+            echo -e "   \e[1;32mPHP 7.3 Service is going to install in the server: ----  \e[0m  "
             echo "#############################################################" 
             ./php7.3.sh
             ;;
         "Database Service (MySql)")
             echo "#############################################################"
-            echo "       MySql Service is going to install in the server   "
+            echo -e "   \e[1;32mMySql Service is going to install in the server: ----  \e[0m  "
             echo "#############################################################"
             ./mysql.sh
             ;;
         "Database Service (MariaDB)")
             echo "#############################################################"
-            echo "       MariaDB Service is going to install in the server     "
+            echo -e "   \e[1;32mMariaDB Service is going to install in the server: ----  \e[0m  "
             echo "#############################################################" 
             ./mariadb.sh
             ;;
