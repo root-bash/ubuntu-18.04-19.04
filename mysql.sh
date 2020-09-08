f0fdf145ef617fae6ed4efc89c099b6f77af9506
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-09-08 02:22:49 -0400

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -43,11 +43,11 @@ echo -n -e "  \e[1;32mPlease Enter the database name: ----  \e[0m"; read DB
 echo ""
 
 # To create a Database. 
-mysql -u USER -p$PASSWORD -e "CREATE DATABASE $DB;"
+mysql -u $CREATE_USER_NAME -p$PASSWORD -e "CREATE DATABASE $DB;"
 
 # To assign the permission 
-mysql -u USER -p$PASSWORD -e "CREATE USER '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
-mysql -u USER -p$PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
+mysql -u $CREATE_USER_NAME -p$PASSWORD -e "CREATE USER '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
+mysql -u $CREATE_USER_NAME -p$PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
 
 echo "---------------------------------------------"
 echo ""
